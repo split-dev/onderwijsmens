d8b5adcc8a04e89fc2773bbfb6265463e7b511a2
Yulia Borzhak <Yuliaborzhak@gmail.com>
2020-02-21 13:32:07 +0200

    слайдер

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,8 +1,16 @@
-
+import 'jquery'
+import 'slick'
+import 'slick-carousel'
 
 export default {
   init() {
-
+    $('.feedback__slider-init').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // arrows: true,
+      dots: true,
+    });
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
