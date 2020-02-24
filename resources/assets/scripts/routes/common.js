bfe68544cbf6d9bb8e0a82bf9368f48f60136042
Yulia Borzhak <Yuliaborzhak@gmail.com>
2020-02-24 18:26:17 +0200

    partners slider

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -13,6 +13,14 @@ export default {
       arrows: false,
       fade: true,
     });
+    $('.partners__all').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      // fade: true,
+    });
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
