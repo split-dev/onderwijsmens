6b18b80e54b1d110c2234c07b174f58294413992
Yulia Borzhak <Yuliaborzhak@gmail.com>
2020-03-03 09:56:14 +0200

    +

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -12,25 +12,88 @@ export default {
       $('body').toggleClass('body-hidden');
     });
 
-    let topnav = document.getElementById('top-nav');
-    let logo = document.querySelector('.top-nav__logo > a > img');
-    let linkColor = document.querySelectorAll('.top-nav__menu ul li a');
-    let socialColor = document.querySelectorAll('.top-nav__menu ul li a svg');
-    window.onscroll = function() {
-      if (window.pageYOffset >= 50) {
-        topnav.classList.add('onscroll1');
-        logo.setAttribute('src', 'images/icons/logo-purple.svg');
-        linkColor.classList.add('onscroll-text-color');
-        socialColor.classList.add('onscroll-social-color');
-        // burger.classList.add('onscroll-burger');
-        console.log(1)
+    let bodyTag = $('body');
+    let scholen = $('.top-nav__menu ul li:nth-of-type(1) a');
+    let bestuur = $('.top-nav__menu ul li:nth-of-type(2) a');
+    let overons = $('.top-nav__menu ul li:nth-of-type(3) a');
+    let opleidingen = $('.top-nav__menu ul li:nth-of-type(4) a');
+    let downloads = $('.top-nav__menu ul li:nth-of-type(5)');
+    let contact = $('.top-nav__menu ul li:nth-of-type(6) a');
+    //mobile
+    let scholen2 = $('.mobile-menu__menu ul li:nth-of-type(1) a');
+    let bestuur2 = $('.mobile-menu__menu ul li:nth-of-type(2) a');
+    let overons2 = $('.mobile-menu__menu ul li:nth-of-type(3) a');
+    let opleidingen2 = $('.mobile-menu__menu ul li:nth-of-type(4) a');
+    let downloads2 = $('.mobile-menu__menu ul li:nth-of-type(5)');
+    let contact2 = $('.mobile-menu__menu ul li:nth-of-type(6) a');
+
+    if (bodyTag.hasClass('scholen-1')) {
+      scholen.addClass('top-nav-interaction');
+      scholen2.addClass('mobile-menu-interaction');
+    }
+    if (bodyTag.hasClass('scholen-2')) {
+      bestuur.addClass('top-nav-interaction');
+      bestuur2.addClass('mobile-menu-interaction');
+    }
+    if (bodyTag.hasClass('over-ons')) {
+      overons.addClass('top-nav-interaction');
+      overons2.addClass('mobile-menu-interaction');
+    }
+    if (bodyTag.hasClass('opleidingen-1')) {
+      opleidingen.addClass('top-nav-interaction');
+      opleidingen2.addClass('mobile-menu-interaction');
+    }
+    if (bodyTag.hasClass('downloads')) {
+      downloads.addClass('top-nav-interaction');
+      downloads2.addClass('mobile-menu-interaction');
+    }
+    if (bodyTag.hasClass('contact-page')) {
+      contact.addClass('top-nav-interaction');
+      contact2.addClass('mobile-menu-interaction');
+    }
+
+    let topnav = $('#top-nav');
+    let logo = $('.top-nav__logo > a > img');
+    let linkColor = $('.top-nav__menu ul li a');
+    let socialColor = $('.top-nav__menu ul li a svg');
+    $(window).scroll(function() {
+      if ($(this).scrollTop() >= 10) {
+        topnav.addClass('onscroll1');
+        linkColor.addClass('onscroll-text-color');
+        socialColor.addClass('onscroll-social-color');
+        logo.attr('src', 'images/icons/logo-purple.svg');
+        if (bodyTag.hasClass('scholen-1')) {
+          scholen.addClass('top-nav-interaction-onscroll');
+        }
+        if (bodyTag.hasClass('scholen-2')) {
+          bestuur.addClass('top-nav-interaction-onscroll');
+        }
+        if (bodyTag.hasClass('over-ons')) {
+          overons.addClass('top-nav-interaction-onscroll');
+        }
+        if (bodyTag.hasClass('opleidingen-1')) {
+          opleidingen.addClass('top-nav-interaction-onscroll');
+        }
+        if (bodyTag.hasClass('downloads')) {
+          downloads.addClass('top-nav-interaction-onscroll');
+        }
+        if (bodyTag.hasClass('contact-page')) {
+          contact.addClass('top-nav-interaction-onscroll');
+        }
       } else {
-        topnav.classList.remove('onscroll1');
-        logo.setAttribute('src', 'images/icons/logo.svg');
-        linkColor.classList.remove('onscroll-text-color');
-        socialColor.classList.remove('onscroll-social-color');
+        topnav.removeClass('onscroll1');
+        linkColor.removeClass('onscroll-text-color');
+        socialColor.removeClass('onscroll-social-color');
+        logo.attr('src', 'images/icons/logo.svg');
+        scholen.removeClass('top-nav-interaction-onscroll');
+        bestuur.removeClass('top-nav-interaction-onscroll');
+        overons.removeClass('top-nav-interaction-onscroll');
+        opleidingen.removeClass('top-nav-interaction-onscroll');
+        downloads.removeClass('top-nav-interaction-onscroll');
+        contact.removeClass('top-nav-interaction-onscroll');
       }
-    };
+    });
+
 
     $('.feedback__slider-init').slick({
       infinite: true,
@@ -81,46 +144,6 @@ export default {
     $('#contact-select').select2({
       placeholder: 'Stuur mij informatie over...',
     });
-
-    let bodyTag = $('body');
-    let scholen = $('.top-nav__menu ul li:nth-of-type(1) a');
-    let bestuur = $('.top-nav__menu ul li:nth-of-type(2) a');
-    let overons = $('.top-nav__menu ul li:nth-of-type(3) a');
-    let opleidingen = $('.top-nav__menu ul li:nth-of-type(4) a');
-    let downloads = $('.top-nav__menu ul li:nth-of-type(5)');
-    let contact = $('.top-nav__menu ul li:nth-of-type(6) a');
-    //mobile
-    let scholen2 = $('.mobile-menu__menu ul li:nth-of-type(1) a');
-    let bestuur2 = $('.mobile-menu__menu ul li:nth-of-type(2) a');
-    let overons2 = $('.mobile-menu__menu ul li:nth-of-type(3) a');
-    let opleidingen2 = $('.mobile-menu__menu ul li:nth-of-type(4) a');
-    let downloads2 = $('.mobile-menu__menu ul li:nth-of-type(5)');
-    let contact2 = $('.mobile-menu__menu ul li:nth-of-type(6) a');
-
-    if (bodyTag.hasClass('scholen-1')) {
-      scholen.addClass('top-nav-interaction');
-      scholen2.addClass('mobile-menu-interaction');
-    }
-    if (bodyTag.hasClass('scholen-2')) {
-      bestuur.addClass('top-nav-interaction');
-      bestuur2.addClass('mobile-menu-interaction');
-    }
-    if (bodyTag.hasClass('over-ons')) {
-      overons.addClass('top-nav-interaction');
-      overons2.addClass('mobile-menu-interaction');
-    }
-    if (bodyTag.hasClass('opleidingen-1')) {
-      opleidingen.addClass('top-nav-interaction');
-      opleidingen2.addClass('mobile-menu-interaction');
-    }
-    if (bodyTag.hasClass('downloads')) {
-      downloads.addClass('top-nav-interaction');
-      downloads2.addClass('mobile-menu-interaction');
-    }
-    if (bodyTag.hasClass('contact-page')) {
-      contact.addClass('top-nav-interaction');
-      contact2.addClass('mobile-menu-interaction');
-    }
 
     // let burger = document.getElementById('burger');
     // window.onscroll = function() {
